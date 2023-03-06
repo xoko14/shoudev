b07fa0a9fed38f8eff53189955d5fdb7b4679c18
xoko14 <xoquinperezb@gmail.com>
2023-03-06 20:06:57 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::net::SocketAddr;
+
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main(){
+   tracing_subscriber::fmt::init();
+   
+   let app = Router::new()
+       .route("/", get(root));
+
+   let addr = SocketAddr::from(([127,0,0,1], 3000));
+   tracing::debug!("listening on {}", addr);
+   axum::Server::bind(&addr)
+       .serve(app.into_make_service())
+       .await
+       .unwrap();
+}
+
+async fn root() -> &'static str{
+    "hello axum!"
+}
